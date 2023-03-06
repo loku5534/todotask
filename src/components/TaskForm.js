35c62c72e3afcbdd23a1a1508aea82cd19d68f7d
luku5534 <luku5534>
2023-03-06 17:47:28 +0500

    don

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -26,7 +26,7 @@ function TaskForm({handler}) {
                         <Button type="submit" variant="primary">Add Task</Button>
                     </Col>
                 </Row>
-                <h6>© 2023 By Baldino | All Rights Reserved | Fateh Don</h6>
+                <h6>© 2023 By Baldino | All Rights Reserved | Fateeh Don</h6>
             </Form>
     )
 }
